86de9eed5ed5c3b8e0e736d72f48e317962a5e28
ZYecho <zy675793960@yeah.net>
2017-03-21 20:28:19 +0800

    fix issue "can't find cu-cli.jar" and rm compile.sh.

--- cu-cli/compile.sh
+++
@@ -1,4 +0,0 @@
-
-mvn clean install eclipse:eclipse -Dmaven.test.skip=true
-
-mvn install:install-file -DgroupId=fr.treeptik -DartifactId=cu-cli -Dversion=1.9 -Dpackaging=jar -Dfile=target/CloudUnitCLI.jar
